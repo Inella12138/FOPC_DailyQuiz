79723d0c15b4a49791341fb232b4e90883d550b2
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-07 13:52:19 +0800

    Day 3 Quiz

---
+++ Day_3_Quiz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Quiz
+{
+    internal class Day_3_Quiz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ISS Gadget Shop!");
+            Console.Write("Numbers of items to purchase: ");
+            double value = 500*int.Parse(Console.ReadLine());
+            if (value > 2000 && value <= 3000) { value = value * 0.97; }
+            if (value > 3000 && value <= 6000) { value = value * 0.95; }
+            if (value > 6000) { value = value * 0.92; }
+            Console.WriteLine($"\n Please pay ${value:0.00}");
+        }
+    }
+}
