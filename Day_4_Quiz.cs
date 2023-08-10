4ed419dd59dd6054bd55abe4bcd3c3a33c890fa5
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-10 14:10:15 +0800

    updated

---
+++ Day_4_Quiz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Quiz
+{
+    internal class Day_4_Quiz
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to the Bank of ISS");
+            string PIN = "192837";
+            int i = 1;
+            while (i < 4) 
+            {
+                Console.WriteLine("Enter your PIN: ");
+                string user =Console.ReadLine();
+                if (user.Equals(PIN)) 
+                {
+                    Console.WriteLine("PIN accepted. You can access your account now");
+                    break;
+                }
+                else
+                {
+                    if (i == 3)
+                    {
+                        Console.WriteLine("Too many wrong PIN entries. Your account is now locked");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect PIN. Please try again");
+                    }
+                }               
+                i++;
+            }
+        }
+    }
+}
---
+++ Day_5_Quiz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Quiz
+{
+    internal class Day_5_Quiz
+    {
+        static void Main(string[] args)
+        {
+            int[,] theArray = new int[,]
+            {
+                {5,3},{2,9},{2,9},
+                {5,1},{7,1},{8,3},
+                {0,3},{2,2},{9,7}
+            };
+
+            int count = 0;
+
+            Console.Write("Please enter a number (0-9): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < theArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < theArray.GetLength(1); j++)
+                {
+                    if (theArray[i,j] == input)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count != 0)
+            {
+                Console.WriteLine("The number {0} appeared for {1} times.",input, count);
+            }
+            else { Console.WriteLine("The number {0} did not appeared.",input); }
+            
+        }
+
+    }
+}
